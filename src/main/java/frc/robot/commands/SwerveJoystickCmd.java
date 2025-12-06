dead3fab3f808e08caa6aa9378c2c2aeb9bc8c52
Alik9151 <91481295+Alik9151@users.noreply.github.com>
2025-12-06 01:13:51 -0500

    UHOH THAT WOULD HAVE RAN INTO A WALL
    whoopsie!

--- src/main/java/frc/robot/commands/SwerveJoystickCmd.java
+++ src/main/java/frc/robot/commands/SwerveJoystickCmd.java
@@ -66,7 +66,7 @@ public class SwerveJoystickCmd extends Command {
         // 5. Convert chassis speeds to individual module states
         SwerveModuleState[] moduleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
         
-        SwerveDriveKinematics.desaturateWheelSpeeds(moduleStates, DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+        SwerveDriveKinematics.desaturateWheelSpeeds(moduleStates, DriveConstants.kTeleDriveMaxSpeedMetersPerSecond);
         // 6. Output each module states to wheels
         swerveSubsystem.setModuleStates(moduleStates);
     }
